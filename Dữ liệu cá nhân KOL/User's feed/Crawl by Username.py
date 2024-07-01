b3a698820173ff90f4d393e23c10f9c0ec04ccbf
QuangHoan3112 <quanghoan10930@gmail.com>
2024-07-01 13:39:16 +0700

    Update Crawl by Username.py

---
+++ Dữ liệu cá nhân KOL/User's feed/Crawl by Username.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+# Nhập nickname của người dùng từ input
+nickname = input("Nhập nickname của người dùng: ")
+
+# Nhập số lượng truy vấn từ input
+max_cursor = input("Nhập số lượng truy vấn: ")
+
+# URL API
+url = f"https://tiktok-scrapper-videos-music-challenges-downloader.p.rapidapi.com/user/{nickname}/feed"
+
+# Thông tin querystring
+querystring = {"max_cursor": max_cursor}
+
+# Headers
+headers = {
+    "x-rapidapi-key": "6b6ca08709mshc0be3cb9d1c6b4bp11a949jsnc754d60f18a3",
+    "x-rapidapi-host": "tiktok-scrapper-videos-music-challenges-downloader.p.rapidapi.com"
+}
+
+# Gửi request và nhận response
+response = requests.get(url, headers=headers, params=querystring)
+
+# Kiểm tra mã trạng thái của response
+if response.status_code == 200:
+    # Chuyển đổi response thành JSON
+    data = response.json()
+    
+    # Lưu dữ liệu JSON vào file
+    output_file = f"tiktok_{nickname}_feed.json"
+    with open(output_file, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=4)
+    print(f"Dữ liệu đã được lưu vào file {output_file}")
+else:
+    print("Yêu cầu API không thành công. Mã trạng thái:", response.status_code)
+    print("Nội dung response:", response.text)
